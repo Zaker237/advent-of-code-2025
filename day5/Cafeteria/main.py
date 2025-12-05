220a28695813c05b21942fecce23733c0f3ff04e
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2025-12-05 23:05:57 +0100

    day5: Part 1

---
+++ day5/Cafeteria/main.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+
+INPUTS_FILE_PATH = Path("inputs.txt")
+
+
+def read_file_lines(file_path: Path) -> list[str]:
+    if not file_path.exists():
+        raise FileNotFoundError()
+
+    with open(file_path) as fp:
+        file_lines = [
+            item[:-1]
+            for item in fp.readlines()
+            if len(item) > 0 and item.endswith("\n") and item != "\n"
+        ]
+    return file_lines
+
+
+def solve_part1(file_path: Path) -> list[int]:
+    databases_items = read_file_lines(file_path)
+    if not databases_items:
+        raise Exception(f"the file {file_path.stem} do not contains databases_items")
+    # databases_items = [
+    #     "3-5",
+    #     "10-14",
+    #     "16-20",
+    #     "12-18",
+    #     "",
+    #     "1",
+    #     "5",
+    #     "8",
+    #     "11",
+    #     "17",
+    #     "32",
+    # ]
+    ingredient_ids = []
+    ranges = []
+    fresh_ingredient_ids = []
+    for line in databases_items:
+        if not line:
+            continue
+        if "-" in line:
+            ranges.append(line)
+        else:
+            ingredient_ids.append(int(line))
+
+    for ingredient in ingredient_ids:
+        for interval in ranges:
+            start = int(interval.split("-")[0])
+            end = int(interval.split("-")[1])
+            if ingredient >= start and ingredient <= end:
+                if ingredient not in fresh_ingredient_ids:
+                    fresh_ingredient_ids.append(ingredient)
+    # print(fresh_ingredient_ids)
+    return fresh_ingredient_ids
+
+
+def main():
+    fresh_ingredients_1 = solve_part1(INPUTS_FILE_PATH)
+    print(f"Ihe total output joltage is: {len(fresh_ingredients_1)}")
+
+
+if __name__ == "__main__":
+    main()
