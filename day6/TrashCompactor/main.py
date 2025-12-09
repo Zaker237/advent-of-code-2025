fe96bfc97b8a257c624ddd961856c76df9d51c7c
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2025-12-09 10:44:32 +0100

    day6: part 2

--- day6/TrashCompactor/main.py
+++ day6/TrashCompactor/main.py
@@ -17,6 +17,19 @@ def read_file_lines(file_path: Path) -> list[str]:
     return file_lines
 
 
+def transpose_strings(strings):
+    grid = [list(row) for row in strings]
+    # Determine max width to pad all rows equally
+    max_len = max(len(row) for row in grid)
+
+    for row in grid:
+        row.extend(" " * (max_len - len(row)))
+
+    transposed = ["".join(grid[r][c] for r in range(len(grid))) for c in range(max_len)]
+
+    return transposed
+
+
 def solve_part1(file_path: Path) -> list[int]:
     sheets = read_file_lines(file_path)
     if not sheets:
@@ -48,7 +61,55 @@ def solve_part1(file_path: Path) -> list[int]:
 
 
 def solve_part2(file_path: Path) -> list[int]:
-    return 0
+    sheets = read_file_lines(file_path)
+    if not sheets:
+        raise Exception(f"the file {file_path.stem} do not contains sheets data")
+    # sheets = [
+    #     "123 328  51 64 ",
+    #     " 45 64  387 23 ",
+    #     "  6 98  215 314",
+    #     "*   +   *   +  ",
+    # ]
+    symbols = [val for val in sheets[-1].split(" ") if val and val != " "]
+    operations = {
+        str(index): {"operator": symbol, "values": []}
+        for index, symbol in enumerate(symbols, 1)
+    }
+    symbol_sheet = sheets[-1]
+    for line in sheets[:-1]:
+        values = [val for val in line.split(" ") if val and val != " "]
+        if len(values) != len(symbols):
+            continue
+        for idx, item in enumerate(values, 1):
+            operations[f"{idx}"]["values"].append(int(item))
+    moves = []
+    for key in operations:
+        operations[key]["length"] = len(str(max(operations[key]["values"])))
+        moves.append(len(str(max(operations[key]["values"]))))
+    _operations = []
+    result = 0
+    for idx, line in enumerate(sheets[::-1]):
+        reverted_line = line[::-1]
+        if idx != 0:
+            _operations.append(reverted_line)
+    transposed_values = [val[::-1] for val in transpose_strings(_operations)]
+    current_pos = 0
+    final_operations = {}
+    for pos, move in enumerate(moves[::-1], 1):
+        values_block = transposed_values[current_pos : current_pos + move]
+        symbol_block = symbol_sheet[::-1][current_pos : current_pos + move]
+        current_pos += move + 1
+        final_operations[str(pos)] = {
+            "operator": symbol_block.strip(),
+            "values": [int(item.strip()) for item in values_block],
+        }
+    result = 0
+    for key in final_operations.keys():
+        if final_operations[key]["operator"].strip() == "+":
+            result += sum(final_operations[key]["values"])
+        else:
+            result += math.prod(final_operations[key]["values"])
+    return result
 
 
 def main():
